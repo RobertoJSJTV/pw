48b17ef365b476d5b31b5fa9b535125392a67451
Roberto José <91293740+RobertoJSJTV@users.noreply.github.com>
2022-06-08 17:21:33 -0300

    pagina imovel quase feita

--- js/imovel.js
+++ js/imovel.js
@@ -1,8 +1,8 @@
-const imgs = document.querySelector('.img select a');
-const imgBtns = [... imgs];
+const imgs = document.querySelectorAll('.img-select a');
+const imgBtns = [...imgs];
 let imgId = 1;
 
-imgBtns.forEach((imgItem) =>{
+imgBtns.forEach((imgItem) => {
     imgItem.addEventListener('click', (event) => {
         event.preventDefault();
         imgId = imgItem.dataset.id;
@@ -13,7 +13,7 @@ imgBtns.forEach((imgItem) =>{
 function slideImage(){
     const displayWidth = document.querySelector('.img-showcase img:first-child').clientWidth;
 
-    document.querySelector('.img-showcase').getElementsByClassName.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+    document.querySelector('.img-showcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
 }
 
-window.addEventListener('resize',slideImage);
+window.addEventListener('resize', slideImage);
