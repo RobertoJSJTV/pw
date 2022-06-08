e3c20d0ab1235bd9e75f8ba104fdae1503172bfc
melqui1998 <melque2012@gmail.com>
2022-06-07 21:53:13 -0300

    Login e Cadastr em LocalStorage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,86 @@
+
+// Verifca se está logado
+
+window.onload = function(){
+
+  // if (typeof(localStorage.email) !="undefined"){
+    if(localStorage.logado === 'true'){ 
+    document.getElementById('registrar').text = localStorage.email;
+    document.getElementById('logar').text = "Sair";
+  }else{
+  //  alert('Não Tem E-mail')
+  }
+
+  // if (logado == true){
+  //   document.getElementById('registrar').text = localStorage.email;
+  //   document.getElementById('logar').text = "Sair";
+  // }
+
+
+}
+
+
 function validaEmail() {
   if (!document.fCadastro.txtEmail.value.includes("@")) {
     alert("E-mail Inválido, Por favor Digite um e-mail Válido");
   }
+}
+
+
+var cadastrar = function (){
+  successCadastro();
+  localStorage.setItem('nome', document.getElementById('name').value);
+  localStorage.setItem('email', document.getElementById('email').value);
+  localStorage.setItem('senha', document.getElementById('senha').value);
+  localStorage.setItem(logado, false);
+}
+
+
+var logar = function(){
+  if (document.getElementById('logar').text === 'Sair'){
+    alert ('Tchau')
+    window.localStorage.removeItem('nome')
+    window.localStorage.removeItem('email')
+    window.localStorage.removeItem('senha')
+    localStorage.logado = false;
+    location.reload();
+  
+  }else{
+    window.location.href = "login.html";
+  }
+
+}
+
+  var fazerLogin = function(){
+  if (document.getElementById('email').value === localStorage.email &&
+  document.getElementById('senha').value === localStorage.senha
+  ){
+     alert('Login Sucess')
+     localStorage.logado = true;
+  }else{
+    alert('E-mail ou Senha incorreta')
+    localStorage.logado = false;
+  }
+}
+
+
+
+
+
+
+function successCadastro(){
+  var r = confirm('Usuário Cadastrado! Faça Login com seu E-mail e Senha!');
+  if (r == true){
+    //Deveria Voltar Pra Index Esse Lixo
+    window.location.href = "index.html";
+  }
+}
+
+
+function exibir(){
+  alert(
+    'Nome: ' + localStorage.nome +
+    '\nEmail: ' + localStorage.email + 
+    '\nSenha: ' + localStorage.senha
+    );
 }
